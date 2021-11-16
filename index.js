27901300549133950ec2e68f3768091788842f18
Rodrigo Hernandez <Rodrigo.Hernandez@endava.com>
2021-11-16 17:35:57 -0300

    k

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const fs = require('fs');
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+var log = "lorem asdjhaskjdhas asd ad sa das dasd sad sadasjd asdmnsa ds das  dasd sad as d \n";
+
 app.get('/', (req, res) => {
-  res.send('Hello World! Este es el ambiente de ' + process.env.ENVIROMENT)
-})
+
+  fs.appendFile("./log/archivo_log.log", log , function (err) {
+    if (err) {
+      throw err;
+    }
+    res.send("Me conectare con el servicio: \n\nHOST: " + process.env.HOST + "\nPORT: " + process.env.PORT)
+  })
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
